9e89fa476dc87402d1ac660234bcf6edc343a8b8
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2025-05-14 22:15:29 +0600

    addaed array

---
+++ Day-02/array.js
@@ -0,0 +1,22 @@
+
+// Using array literal notation (most common)
+
+let array = [] ; // empty array
+let numbers=[1,2,3,4,6]; // arary with initial value
+
+let mixedArray= ["Alauddin", 25, true, null]; //mixrd arary
+
+// Using the Array constructor
+
+let anotherArray= new Array(); // empty array
+
+let sizedArray= new Array(5);// Array with a specified size (initially empty elements)
+sizedArray.push(1,2,4,6,7,8)
+let populatedArray= new Array(1,2,4,5)
+
+console.log(array)
+console.log(numbers)
+console.log(mixedArray)
+console.log(anotherArray)
+console.log(sizedArray)
+console.log(populatedArray)
