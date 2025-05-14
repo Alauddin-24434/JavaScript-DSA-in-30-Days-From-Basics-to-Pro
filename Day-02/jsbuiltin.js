5e68b8083a36154b39fea2939b1cda75cbfe5601
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2025-05-14 22:39:11 +0600

    added loop , builtdin method

---
+++ Day-02/jsbuiltin.js
@@ -0,0 +1,17 @@
+
+// date 
+
+const date= new Date();
+
+console.log(date) // 2025-05-14T16:30:37.065Z
+console.log(date.toUTCString());//Wed, 14 May 2025 16:30:37 GMT
+
+
+console.log(date.getFullYear()); // 2025
+
+
+console.log(date.getHours()); // now hours
+console.log(date.getMinutes());  // mongth
+
+console.log(date.getSeconds())//seconds
+
---
+++ Day-02/loop.js
@@ -0,0 +1,17 @@
+//  for in loop
+
+const person= {name:"alauddin", age:25, address:{city:"Barisal", village:"Mongolhata"}}
+
+
+for( let key in person){
+    console.log(key)
+}
+
+    const colors = ["red", "green", "blue"];
+    for (let color of colors) {
+      console.log(color);
+    }
+        const numbers = [1, 2, 3, 4, 5];
+    numbers.forEach(function(number) {
+      console.log(number);
+    });
